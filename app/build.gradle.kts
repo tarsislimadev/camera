844d547740485c0362e657c17eeb8de3a2a412d8
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2025-12-26 15:13:36 -0300

    Fix Gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,12 @@ plugins {
 
 android {
     namespace = "com.tarsislimadev.camera"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.tarsislimadev.camera"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
